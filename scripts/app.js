b8dbd6140719e3a91b95b0381e54876553e25197
dazeyb <btdazey@gmail.com>
2021-04-05 18:46:00 -0700

    Working on user story

---
+++ scripts/app.js
@@ -0,0 +1,54 @@
+User story:
+
+Stage 1: 
+
+
+Player will see cell-phone shaped box that is styled.
+
+Player will click a button on the screen and entering name, and the game will begin. 
+
+Name of monster will === entered name, shown somewhere.
+
+An egg will appear on the screen, one life bar, one button for radiation.
+
+Age will appear, starting at 0.
+
+A timer will start for age, and one will start for health bar.
+
+Health bar will decrease over time.
+
+If health bar = 0, end game and say "Name has died"
+
+Clicking radiation button will increase health.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Create a repo for your tomagotchi pet
+make a commit after you finish each one of the following
+Instatiate your Tomagotchi
+Display a character of your choice on the screen to represent your pet
+Display the following metrics for your pet:
+Radiation (1-10 scale)
+Sleepiness (1-10 scale)
+Boredom (1-10 scale)
+Age
+Add buttons to the screen to feed your pet, turn off the lights, and play with your pet.
+Add the ability to name your pet.
+Style the page.
+Increase your pet's age every x minutes
+Increase your pet's Hunger, Sleepiness, and Bored metrics on an interval of your choosing.
+You pet should die if Hunger, Boredom, or Sleepiness hits 10.
+Morph your pet at certain ages.
+Animate your pet across the screen while it's alive.
